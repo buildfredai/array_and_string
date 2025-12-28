df5a430ca645edb978489a9b780d6352273e1d12
buildfredi <fred.aiversion@gmail.com>
2025-12-28 18:33:41 +0530

    Initial code

--- 2-d_array_2_3_table.c
+++ 2-d_array_2_3_table.c
@@ -3,15 +3,14 @@ int main(){
     int arr [2][10];
     for(int i = 0 ; i < 2 ; i++){
         for( int j=0 ; j < 10 ; j++){
-            printf("%d * %d = ", i+2,j+1);
-            scanf("%d",&arr[i][j]);
+            arr[i][j] = (i+2)*(j+1);
         }
-        printf("\n");
     }
     for(int i = 0 ; i < 2 ; i++){
         for( int j = 0 ; j < 10 ; j++){
             printf("%d * %d = %d\n",i+2,j+1,arr[i][j]);
         }
+        printf("\n");
     }
     return 0;
 }
